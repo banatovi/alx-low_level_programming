1f758b316ac8e4873d83e7beecf3c1d6f16e2b6a
banatovi <anatovib@gmail.com>
2023-07-14 21:24:41 +0100

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 0 when everything works fine
+*
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 150; n < 160; ++n)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
