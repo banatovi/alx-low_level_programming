14fb0bcaad2e2e078d598261053f1f01cffdec4a
banatovi <anatovib@gmail.com>
2023-07-18 20:37:17 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
-#include "main.h"
+#include <"stdio">
+#include <"main.h">
 /**
 *print_alphabet_x10 - prints alphabet letters
 * ten times
 *desc: The function uses loop to operate
 *Return: no return
 */
-void print_alphabet_x10(void)
+int main void print_alphabet_x10(void)
 {
 int counter = 0;
 int letter;
