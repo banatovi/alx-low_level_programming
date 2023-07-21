31013f2a6ebc1f750dc3c9989135125e1533e3dd
banatovi <anatovib@gmail.com>
2023-07-21 01:34:22 +0100

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,18 +6,22 @@
 * @n: The number to print
 */
 
-void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		putchar('-');
-		num = -num;
-	}
-	if (num / 9)
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-		print_number(num / 10);
+		n1 = n;
 	}
-	putchar((num % 10) + '0');
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
