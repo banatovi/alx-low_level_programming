040be33633d9bcff3556add6e0cd62c369f86333
banatovi <anatovib@gmail.com>
2023-07-27 02:46:14 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
-* _strncpy - copy a string
-* @n: input value
-* @src: input value
-* @dest: input value
-* Return: dest
-*/
+ *_strncat - concatenate two strings but add inputted number of bytes
+ *@n:integer parameter to compare index to
+ *@src:string to be completed at end of dest
+ *@dest:string to be appended upon
+ *Return:returns new concatenated string
+ */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int j;
 
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[j] = src[j];
-		j++;
-	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
+
 }
