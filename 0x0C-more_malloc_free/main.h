6ba975e8d8283fe91890dd5a08c5970d7d8fe0d7
banatovi <anatovib@gmail.com>
2023-10-09 00:23:11 +0100

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -14,5 +14,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
+int main(int argc, char const *argv[]);
 
 #endif /*MAIN_H*/
